5296f38b0a59319b6899a52e5148a025ca0b6231
algoritmika-ekb <pashamikov@hotmail.com>
2026-04-19 16:34:04 +0500

    Update main.py

--- main.py
+++ main.py
@@ -48,5 +48,26 @@ font = font.Font(None, 35)
 lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
 lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
 
+speed_x = 3
+speed_y = 3
+
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+        if finish != True:
+            window.fill(back)
+            racket1.update_l()
+            racket2.update_r()
+            ball.rect.x += speed_x
+            ball.rect.y += speed_y
+
+            racket1.reset()
+            racket2.reset()
+            ball.reset()
+        display.update()
+        clock.tick(FPS)
+
+
 
 
