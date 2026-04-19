3e57c03c15517338e1b30356c54bb1f0ec4b4185
algoritmika-ekb <pashamikov@hotmail.com>
2026-04-19 16:24:35 +0500

    Update main.py

--- main.py
+++ main.py
@@ -33,3 +33,20 @@ class Player(GameSprite):
 back = (200, 255, 255)
 window = display.set_mode((600, 500))
 window.fill(back)
+
+game = True 
+finish = False
+clock = clock.Clock()
+FPS = 60
+
+racket1 = Player('racket.png', 30, 200, 4, 50, 150)
+racket2 = Player('racket.png', 520, 200, 4, 50, 150)
+ball = GameSprite('tenis_baall.png', 200, 200, 4, 50, 50)
+
+font.init()
+font = font.Font(None, 35)
+lose1 = font.render('PLAYER 1 LOSE!', True, (180, 0, 0))
+lose2 = font.render('PLAYER 2 LOSE!', True, (180, 0, 0))
+
+
+
